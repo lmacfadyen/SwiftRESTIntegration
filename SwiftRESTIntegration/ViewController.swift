743e149253c425229ddeaa081918abceabc5612c
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2018-03-07 10:42:24 -0500

    Initial Commit

---
+++ SwiftRESTIntegration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftRESTIntegration
+//
+//  Created by Lawrence F MacFadyen on 2018-03-07.
+//  Copyright © 2018 Lawrence F MacFadyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
